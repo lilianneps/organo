35d0cf2d21a02b6ab62914547b4d79581bf12f8c
Lilianne Maia <lilianneps@gmail.com>
2025-06-02 10:20:38 -0300

    dc

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -3,11 +3,15 @@ import './CampoTexto.css'
 const CampoTexto = (props) => {
 
     const placeholderModificado = `${props.placeholder}...`
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
         return (
         <div>
             <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder ={placeholderModificado}/>
+            <input value = {props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder ={placeholderModificado}/>
             </div>
         </div>
     )
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,6 +1,7 @@
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
 import Botao from '../Botao';
+import React, { useState } from 'react';
 import './Formulario.css';
 
 const Formulario = () => {
@@ -14,26 +15,50 @@ const Formulario = () => {
         'Inovação e Gestão'
     ]
 
+    const [nome, setNome] = useState('');
+    const [cargo, setCargo] = useState('');
+    const [imagem, setImagem] = useState('');
+
     const aoSalvar = (evento) => {
         evento.preventDefault();
-        console.log('Formulário foi submetido');
+        console.log('Formulário foi submetido' , nome, cargo, imagem);
     }
 
-        return(
-            <section className="formulario">
-                <form onSubmit={aoSalvar}> 
-                    <h2>Preencha os dados do colaborador</h2>
-                    <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
-                    <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
-                    <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-                    <ListaSuspensa obrigatorio={true} label = "Times" itens = {times}  />
-                    <Botao>
-                        Criar Card
-                    </Botao> 
+    return (
+        <section className="formulario">
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados do colaborador</h2>
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Nome"
+                    placeholder="Digite seu nome"
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+                <CampoTexto 
+                obrigatorio={true} 
+                label="Cargo" 
+                placeholder="Digite seu cargo" 
+                valor={cargo}
+                aoAlterado={valor => setCargo(valor)}
+                />
+                <CampoTexto 
+                label="Imagem" 
+                placeholder="Digite o endereço da imagem" 
+                valor={imagem}
+                aoAlterado={valor => setImagem(valor)}  
+                />
+                <ListaSuspensa 
+                obrigatorio={true} 
+                label="Times" 
+                itens={times} />
+                <Botao>
+                    Criar Card
+                </Botao>
 
-                </form>
-            </section>
+            </form>
+        </section>
 
-        )
+    )
 }
 export default Formulario
